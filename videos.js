c0e0c66fff5f175a5f850e22b6d3d12cebdbace1
tyroowedit <298542771+tyroowedit@users.noreply.github.com>
2026-07-20 17:26:19 -0300

    Update videos.js

--- videos.js
+++ videos.js
@@ -16,7 +16,7 @@
 const dexloreVideos = [
   { youtubeId: "9u5GndwppCY", wide: false, title: "POKÉMON quase foi CANCELADO?" },
   { youtubeId: "DGgPW7bU0GE", wide: false, title: "por que o PSYDUCK vive com DOR DE CABEÇA?" },
-  { youtubeId: "YTTE3neBc-U", wide: false, title: "como o MEOWTH aprendeu a FALAR?" },
+  { youtubeId: "IkaKLgi3d3E", wide: false, title: "o POKÉMON que não deveria EXISTIR?" },
   { youtubeId: "lo3e8WGOTLA", wide: false, title: "por que existe pokémon SHINY?" },
   { youtubeId: "NvbAVGkmnB4", wide: false, title: "o que REALMENTE acontece quando um pokémon MORRE" },
   { youtubeId: "vl_rXm7VSHU", wide: false, title: "POR QUE o PIKACHU do ash NUNCA evoluiu" },
@@ -26,6 +26,5 @@ const generalEdits = [
   { youtubeId: "f4t1kZp1X2Y", wide: false, title: "FÃ EDIT BISTECONE" },
    { youtubeId: "4ea1hvMmEE0", wide: false, title: "@YellowPack" },
   { youtubeId: "vZqRYxMWpbc", wide: true, title: "FÃ EDIT JOGANDOFODACCI" },
-  { youtubeId: "escDeFANyf8", wide: true, title: "Video de apresentação antigo(PROMOÇÂO NÃO VALE MAIS)" },
-  { youtubeId: "rEgeqz-d68o", wide: true, title: "Edição para @Allka(video não mais disponível)" },
+  { youtubeId: "Bpa_iLFk2DE", wide: true, title: "Edição Finanças" },
 ];
